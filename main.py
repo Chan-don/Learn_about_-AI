242d2eb021b92011edeecaeced766993f18ac177
ChanDon <idonchan110@gmail.com>
2024-02-21 23:29:30 +0900

    ai 5

--- main.py
+++ main.py
@@ -1,37 +1,32 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
+from langchain.callbacks import StreamingStdOutCallbackHandler
+#callbacks is the live streaming the chat answer while run code
 
 chat = ChatOpenAI(
-    temperature=0.1
-    #temperature valuse high == creativity is high. 
+    temperature=0.1, streaming=True, callbacks=[StreamingStdOutCallbackHandler()]
+    #temperature valuse high == creativity is high.
+    #streaming is live streaming. so, streaming = True means run streaming.
+    # and callbacks code is to combine the streaming code
 )
 
-#-- plus code --
-
-from langchain.schema import BaseOutputParser
-
-class CommaOutputParser(BaseOutputParser):
-
-    def parse(self, text):
-        items = text.strip().split(",")
-        return list(map(str.strip,items))
-
-# this code is how to run the OutputParser.
-#-- plus code --
-
-template1 = ChatPromptTemplate.from_messages([
-    ("system", "You are a list generating machine. Every thing you are asked will be answered with a comma, list of max {max_items} in lowercase. DO NOT reply with anything else."),
-    ("human", "{question}"),
+sportsCar_prompt = sports_car_template = ChatPromptTemplate.from_messages([
+    ("system", "You are a best car dealer. You create recommend which sports car is best to me."),
+    ("human", "I want to {car_type} car."),
 ])
 
-# this code is how to use the OutputParser at Ai chat.
+sports_car_chain = sportsCar_prompt | chat 
 #-- plus code --
 
-Chain1 = template1|chat|CommaOutputParser()
-Chain1.invoke({
-    "max_items": 5,
-    "question": "What are the sports car model name?"
-})
+bestSportsCar_prompt = ChatPromptTemplate.from_messages([
+    ("system", "You work at a used sports car dealership. So, we have a vehicle that we can recommend to our customers. However, customers don't need detailed information about the car, they just want to know the make and model of the car. You need to sort them into categories and show them to your guests. You should also tell the customer if there is a type of car they want."),
+    ("human", "{car}"),
+])
 
-# | is best code in here, that can divide code each other.
-# So, first you write the each part. and collect to using by '|'code.
+best_sports_car_chain = bestSportsCar_prompt | chat
+
+final_chain = {"car" : sports_car_chain} | best_sports_car_chain
+
+final_chain.invoke({
+    "car_type": "convertible",
+})
