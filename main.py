9e9d0b02040494034494a9d25d0964046ddc727e
ChanDon <idonchan110@gmail.com>
2024-02-28 17:45:20 +0900

    ai10

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts.few_shot import FewShotPromptTemplate
 #FewShotPromptTemplate provieds specific prompt templates. Like, Just Capital letter or Just small letter, space, comma, etc..
 from langchain.callbacks import StreamingStdOutCallbackHandler
 #callbacks is the live streaming the chat answer while run code
-from langchain.prompts import PromptTemplate
-from langchain.prompts.example_selector.base import BaseExampleSelector
+from langchain.prompts import load_prompt
+#load prompt at the prompt.json or prompt.yaml
+
+#prompt = load_prompt("./prompt.json")
+# Upper code is Get prompt at the "prompt.json"
+
+prompt = load_prompt("./prompt.yaml")
+# Upper code is Get prompt at the "prompt.yaml"
+# What is different at the json and yaml?
+# yaml is easier to use then json.
 
 chat = ChatOpenAI(
     temperature=0.1, streaming=True, callbacks=[StreamingStdOutCallbackHandler(),],
@@ -13,64 +20,4 @@ chat = ChatOpenAI(
     # and callbacks code is to combine the streaming code
 )
 
-examples = [
-    {
-        "country" : "France",
-        "answer" : """
-        Here is what I know:
-        Capital: Paris
-        Language: French
-        Food: wine and Cheese
-        Currency: Euro
-        """,
-    },
-    {
-        "country" : "Italy",
-        "answer" : """
-        I know this:
-        Capital: Rome
-        Language: Italian
-        Food: pizza and pasta
-        Currency: Euro
-        """,
-    },
-    {
-        "country" : "Korea",
-        "answer" : """
-        I know this:
-        Capital: Seoul
-        Language: Korean
-        Food: Kimki and bulgogi
-        Currency: Won
-        """,
-    },
-]
-
-class RandomExampleSelector(BaseExampleSelector):
-    def __init__(self, examples):
-        self.examples = examples
-
-    def add_example(self, example):
-        self.examples.append(example)
-
-    def select_examples(self, input_varialbes):
-        from random import choice
-
-        return [choice(self.examples)]
-    # Upper Code is get random Examples at 'Example Template'
-
-
-example_prompt = PromptTemplate.from_template("Human: {country}\nAI: {answer}")
-
-example_selector= RandomExampleSelector(
-    examples=examples,
-)
-
-prompt = FewShotPromptTemplate(
-    example_prompt= example_prompt,
-    example_selector=example_selector,
-    suffix="Human: What do you know about {country}?",
-    input_variables=["country"],
-)
-
-prompt.format(country = "Brazil")
+prompt.format(country = "xxx")
