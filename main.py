a0695f8fb8b8ab8048f539bb425781fb744cd407
ChanDon <idonchan110@gmail.com>
2024-02-29 16:55:55 +0900

    ai13

--- main.py
+++ main.py
@@ -1,31 +1,17 @@
 from langchain.chat_models import ChatOpenAI
-#FewShotPromptTemplate provieds specific prompt templates. Like, Just Capital letter or Just small letter, space, comma, etc..
-from langchain.callbacks import StreamingStdOutCallbackHandler
-#callbacks is the live streaming the chat answer while run code
-from langchain.globals import set_llm_cache, set_debug
-#If you use 'cache'. you can save LM(Langauge Model) answer.
-#'debug' is show the log answer. and also show the model name
-from langchain.cache import InMemoryCache, SQLiteCache
-# if you want cache save at the Memory, You use 'SQLiteCache'
-
-#set_llm_cache(InMemoryCache())
-# this is simpe Cache
-
-#set_debug(True)
-# this is debug 
-
-set_llm_cache(SQLiteCache("cache.db"))
-# this is cache save at the Memory. (SQLiteCache("[name]"))
-# if you use this code. 'cache.db' will appear in the 'file list' next to it.
+from langchain.callbacks import get_openai_callback
 
 chat = ChatOpenAI(
     temperature=0.1,
 )
 
-chat.predict("How do you make italian pasta")
-
-# --- next code ---
-
-chat.predict("How do you make italian pasta")
-#This answer is didn't say LLM, Just Loaded at save LM(Langauge Model). We called this 'cache'
-#Why use cache? == Save LLM is Save time is Save Money.
+with get_openai_callback() as usage:
+    a = chat.predict("what is the recipe for soju")
+    b = chat.predict("what is the recipe for beer")
+    print(a, b, "\n")
+    print(usage)
+# if you want to show prise to use.
+# You must use. 'with' 'get_openai_callback()' 'as' usage:
+    # ~~
+    # print(usage)
+# You must use this! for show prise
