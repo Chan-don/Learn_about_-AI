5e7f0f1862fb3d89f6da3892aae64fbc8774c432
ChanDon <idonchan110@gmail.com>
2024-03-05 16:37:53 +0900

    ai 18

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
-# If you store memory at llm model
-# You use 'ConversationSummaryMemory' SummaryMemory 
-# ConverstionSummaryMemory is not just storage.
-# That is to give a summary of the conversations.
-# Initially, SummaryMemory take more token and space more than other conversation.
-# However, as time passes and conversations accumulate, SummaryMemory can be helpful in anwsering.
-from langchain.memory import ConversationSummaryMemory
+# This time, use SummaryMemory by specifying a limit token. 
+from langchain.memory import ConversationSummaryBufferMemory
 from langchain.chat_models import ChatOpenAI
 
-llm = ChatOpenAI(temperature= 0.1)
+llm = ChatOpenAI(temperature=0.1)
 
-memory = ConversationSummaryMemory(llm=llm)
-#llm = llm means this code will be needs money
+memory = ConversationSummaryBufferMemory(
+    llm=llm,
+    max_token_limit=150,
+    # Upper code 'max_token_limit' means limit message.
+    return_messages=True
+)
 
-def add_message(input, output):
-    memory.save_context({"input": input}, {"output": output})
-
-def get_histroy():
-    return memory.load_memory_variables({})
-
-add_message("Hi I'm Chandon, I live in South Korea", "Wow that is so nice!")
+get_history()
 
 # -- next code --
 
-add_message("South Korea is so nice country!", "I want to travel South Korea!")
+add_message("South korea is pretty!", "I want to visit Seoul!")
 
 # -- next code --
 
-get_histroy()
-# This conclusion is a summary of the data I wrote down in memory.
+get_history()
+#Not reach limit yet.
+
+# -- next code --
+
+add_message("How far Seoul from Salt lake city?!", "I don't know! But, that is too far!!!")
+
+# -- next code --
+
+get_history()
+#Not reach limit yet.
+
+# -- next code --
+
+add_message("How many people live Seoul and Salt Lake City?", "I don't know! But, many people are live Seoul and Salt Lake City!")
+# Push multiple times until the limit token is reached.
+
+# -- next code --
+
+get_history()
+# Reach limit token!
+# When consuming tokens up tp the limit tokens.
+# Old data is summarized.
