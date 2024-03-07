e6da6bd05031fce621983bd1ea2019d83482c9f7
ChanDon <idonchan110@gmail.com>
2024-03-07 16:02:52 +0900

    ai 20

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
-# This memory 'ConversationKGMemory' is extracts the most important summary.
-from langchain.memory import ConversationKGMemory
+# Connect memory to the chain.
+# LLM chain == off-the-shelf chain.
+# The off-the-shelf chain refer to general purpose chain. And this chain is very distributed in langchain.
+# off-the-shelf chain is quick to use. However, that cannot customize it.
+
+from langchain.memory import ConversationSummaryBufferMemory
 from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+#Upper chain 'LLMChain' is off-the-shelf chain.
+from langchain.prompts import PromptTemplate
 
-llm = ChatOpenAI(temperature= 0.1,)
-
-memory = ConversationKGMemory(
-    llm=llm,
-    return_messages=True,
+llm = ChatOpenAI(
+    temperature=0.1
 )
 
-def add_message(input, output):
-    memory.save_context({"input" : input}, {"output" : output})
+memory = ConversationSummaryBufferMemory(
+    llm=llm,
+    max_token_limit= 120,
+)
 
-add_message("Hi, my name is Chandon, I live in South Korea", "Wow that is nice!!")
+chain = LLMChain(
+    llm=llm,
+    memory=memory,
+    prompt=PromptTemplate.from_template("{question}"),
+    verbose=True,
+    # This Upper code can show chain prompt log
+)
+
+chain.predict(question="I'm chandon!")
 
 # -- next code --
 
-memory.load_memory_variables({"input" : "who is Chandon?"})
-# why write {"input" : "who is Chandon?"}, That is a way of asking to the memory what it knows about a specific node on the knowledge graph.
+chain.predict(question="I live in yeosu!")
 
 # -- next code --
 
-add_message("Chandon likes jimin", "Wow that is great!")
+chain.predict(question = "what is my name?")
+# Why LLMChain say "I'm Sorry I don't know your name."
+# Because, we didn't update memory!
 
 # -- next code --
 
-memory.load_memory_variables({"input": "Report Chandon."})
+memory.load_memory_variables({})
+# Upper Code is Summary my chat memory.
